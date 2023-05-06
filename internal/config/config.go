7f43c7c1e7a1a00496f2972337cd38008849d9db
Dias12 <diasoryntayev@gmail.com>
2023-05-06 17:16:56 +0600

    update Config: add default value

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,11 +9,11 @@ import (
 )
 
 type Config struct {
-	IsDebug *bool `yaml:"is_debug"`
+	IsDebug *bool `yaml:"is_debug" env-required:"true"`
 	Listen  struct {
-		Type   string `yaml:"type"`
-		BindIp string `yaml:"bind_ip"`
-		Port   string `yaml:"port"`
+		Type   string `yaml:"type" end-default:"port"`
+		BindIp string `yaml:"bind_ip" env-default:"127.0.0.1"`
+		Port   string `yaml:"port" env-default:"8080"`
 	} `yaml:"listen"`
 }
 
@@ -22,6 +22,8 @@ var (
 	once     sync.Once
 )
 
+// this code do one time
+// second time return instance
 func GetConfig() *Config {
 	once.Do(func() {
 		logger := logging.GetLogger()
