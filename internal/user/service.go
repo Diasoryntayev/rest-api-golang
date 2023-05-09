1e76e1a314a398333bf5c02d1ae47bbe2867342c
Dias12 <diasoryntayev@gmail.com>
2023-05-09 20:10:46 +0600

    Preparation service

---
+++ internal/user/service.go
@@ -0,0 +1,16 @@
+package user
+
+// import (
+// 	"context"
+
+// 	"rest/pkg/logging"
+// )
+
+// type Service struct {
+// 	storage Storage
+// 	logger  *logging.Logger
+// }
+
+// func (s *Service) Creagte(ctx context.Context, dto CreateUserDTO) (u User, err error) {
+// 	return
+// }
